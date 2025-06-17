aa87e633865e84179597d45a059fc6983f3cfad0
D1sph0l1dus <76053592+NNNACHID@users.noreply.github.com>
2025-06-17 22:43:57 +0000

    wipwip

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
     <header class="bg-gradient-to-r from-pink-100 to-purple-100 sticky top-0 z-50">
-        <div ></div>
+        <div class="max-w-screen-x1 mx-auto px-4 py-4 flex justify-between items-center">
+            <!-- LOGO -->
+             <a href="/" class="flex items-center"></a>
+        </div>
     </header>
 </template>
 <script></script>
