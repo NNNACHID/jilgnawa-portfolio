9837910e6eb7820e9c6fa7ecf26249703eafa86f
D1sph0l1dus <b043d0n@snakecave.lan>
2025-06-17 22:36:48 +0000

    wip

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted } from 'Vue';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+onMounted(()=>{
+    AOS.init();
+})
+</script>
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+</template>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <header class="bg-gradient-to-r from-pink-100 to-purple-100 sticky top-0 z-50">
+        <div ></div>
+    </header>
+</template>
+<script></script>
