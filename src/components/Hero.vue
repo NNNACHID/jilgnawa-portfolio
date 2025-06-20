26d1e90ad38e903bb2ed50965de6f53ae4ba2421
D1sph0l1dus <76053592+NNNACHID@users.noreply.github.com>
2025-06-20 19:16:59 +0000

    wip hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,20 @@
 <template>
     <section class="relative overflow-hidden bg-gradient-to-r from-pink-100 to-purple-100" id="home">
-        <article class="max-w-screen-xl mx-auto px-4 "></article>
+        <article
+            class="max-w-screen-xl mx-auto px-4 sm:px-8 py-16 md:py-28 flex flex-col-reverse lg:flex-row items-center gap-10 lg:gap-20">
+        </article>
+        <div class="w-full lg:w-1/2 text-center lg:text-left">
+            <hgroup data-aos="fade-up" data-aos-delay="500">
+                <h1 class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-green-800 mb-4">Jil Gnawa</h1>
+                <p class="text-base sm:text-lg text-gray-600 mb-8 max-w-xl mx-auto lg:mx-0">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+            </hgroup>
+        </div>
     </section>
 </template>
 
